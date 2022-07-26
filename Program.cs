25c76253ee32b773053b8a20404b43dcf5b59738
EurisG <euris809@gmail.com>
2022-07-26 13:14:33 -0400

    answer back in mult,div,add and sub

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MyProgram{
+
+    class calc {
+
+        static void Main(string[] args){
+            try {
+                 Console.WriteLine("Lets Calculate (+, -, * And /)");
+            Console.WriteLine("Write the first number..");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Write the second number..");
+            int second = Convert.ToInt32(Console.ReadLine());
+
+            int mult = first * second;
+            int add = first + second;
+            int sub = first - second;
+            int div = first / second;
+
+            Console.WriteLine("Multiplication : " + mult);
+            Console.WriteLine("Addition : " + add);
+            Console.WriteLine("Subtraction : " + sub);
+            Console.WriteLine("Division : " + div);
+
+            }
+            catch(Exception e) {
+                Console.WriteLine(e.Message);
+            }
+            finally{
+                    Console.ReadKey();
+            }
+           
+
+
+
+        }
+    }
+}
